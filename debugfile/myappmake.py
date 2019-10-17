5957bbca095ce3a0cf7ab107fb072a642bfff1f7
YangShugang-bit <56423128+YangShugang-bit@users.noreply.github.com>
2019-10-17 09:16:13 +0800

    new py updata

---
+++ debugfile/myappmake.py
@@ -0,0 +1,13 @@
+# coding = utf-8
+# /usr/yangshugang/Anaconda3 python3.7
+
+'''
+
+Author:Yang Shugang
+Email :yangshugang12@stu.ouc.edu.cn or yangshugang@theidi.com
+Number:18205422083
+date  :2019-10-17 09:15
+introduction:uesd to
+desc
+
+'''
