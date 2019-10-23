a5e4a7f4fea7e597930c950b25edc27efe706787
YangShugang-bit <56423128+YangShugang-bit@users.noreply.github.com>
2019-10-23 08:50:42 +0800

    20191023850

--- debugfile/calculate.py
+++ debugfile/calculate.py
@@ -11,11 +11,13 @@ des
 '''
 
 import numpy as np
+import re
+import linecache
 import pandas as pd
 import sys
 import os
 
-
+#
 # fileDir = "D:/2019工作项目/201909温州洞头大小门岛/sontek/GPS_MODEL_SpringTide083113-083121"
 # for root, dirs, files in os.walk(fileDir):
 #     for dir in dirs:
@@ -24,25 +26,33 @@ import os
 #         print(os.path.join(root, file))  # 文件路径
 #         name,suffix = os.path.splitext(file)  #文件名、后缀名
 def loaddata(filename):
-    file = open(filename)
-    lines = file.readlines()
-    # print lines
-    # ['0.94\t0.81\t...0.62\t\n', ... ,'0.92\t0.86\t...0.62\t\n']形式
-    rows = len(lines)  # 文件行数
-
+    # Try to read a txt file and return a matrix.Return [] if there was a mistake.
+    try:
+        file = open(filename, 'r')
+    except IOError:
+        error = []
+        return error
+    content = file.readlines()
+    print(content)
+    rows = len(content)  # 文件行数
+    print('lines = ',rows)
     datamat = np.zeros((rows, 61))  # 初始化矩阵
+    row_count = 0
 
-    row = 0
-    for line in lines:
-        line = line.strip().split('\t')  # strip()默认移除字符串首尾空格或换行符
-        datamat[row, :] = line[:]
-        row += 1
-        print(datamat)
+    for i in range(1,rows):
+        content[i] = content[i].strip().split('\t')  #去空格 以制表符分隔读取
+        content[i] = [float(xx) for xx in content[i]]
+        datamat[row_count, :] = content[i][:]
+        row_count += 1
 
+    file.close()
     return datamat
 
 
-    fileDir = u"D:/2019工作项目/201909温州洞头大小门岛/sontek/GPS_MODEL_SpringTide083113-083121" \
-              u"/08311930/WENZH1908311930.spd"
+if __name__ == '__main__':
+    fileDir = "D:/2019工作项目/201909温州洞头大小门岛/sontek/GPS_MODEL_SpringTide083113-083121/083120/WENZH1908311958.spd"
     out = loaddata(fileDir)
     print(out)
+
+
+
